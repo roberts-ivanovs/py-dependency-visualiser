4f383a97d5338ef63ca12863b6816f9e5017252d
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-01-17 00:50:13 +0200

    Filtering for python files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use std::fs;
 
+use fs::FileType;
 use structopt::StructOpt;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 mod cli;
 
 fn main() {
     let matches = cli::Opt::from_args();
-    for entry in WalkDir::new(matches.config).into_iter().filter_map(|e| e.ok()) {
-        println!("{}", entry.path().display());
+    for entry in WalkDir::new(matches.config)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !is_hidden(e))
+        .filter(|e| is_python(e))
+    {
+        println!("{} ", entry.path().display());
     }
 
     println!("Hello, world!");
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+fn is_python(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.ends_with("py"))
+        .unwrap_or(false)
 }
